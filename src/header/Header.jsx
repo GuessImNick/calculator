e1bb439e33208f27bb3f96bbbd44fb231f0fcb91
Nicholas Stewart <stewartnick53@icloud.com>
2022-06-07 18:06:45 -0500

    general styling for deployment

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <h1>Calculator App</h1>
+        <p>Project by Nicholas Stewart</p>
+    </header >
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './calculator/Calculator';
+import Header from './header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <Calculator />
   </React.StrictMode>
 );
